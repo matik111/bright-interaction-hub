a35f520987c82f46c46b7709340740b8475840d8
matik111 <haitile509@gmail.com>
2025-02-19 17:56:00 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; // Import QueryClient
 import Index from "./pages/Index";
 import ClientList from "./pages/ClientList";
 import AddClient from "./pages/AddClient";
@@ -6,17 +7,23 @@ import ViewClient from "./pages/ViewClient";
 import NotFound from "./pages/NotFound";
 import "./App.css";
 
+// Create a new instance of QueryClient
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/clients" element={<ClientList />} />
-        <Route path="/clients/add" element={<AddClient />} />
-        <Route path="/clients/:id" element={<ViewClient />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    // Wrap the entire app with QueryClientProvider
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/clients" element={<ClientList />} />
+          <Route path="/clients/add" element={<AddClient />} />
+          <Route path="/clients/:id" element={<ViewClient />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
